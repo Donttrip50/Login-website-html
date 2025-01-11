2249c3d07f7c59638605337b11ee8208da8ac0f9
Donttrip50 <jahdarismith27@gmail.com>
2025-01-11 04:57:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-script.js
+function login() {
+  const username = document.getElementById("login-username").value;
+  const password = document.getElementById("login-password").value;
+
+  if (username === "" || password === "") {
+      alert("Please fill out all fields.");
+      return;
+  }
+
+  alert(`Login attempt with Username: ${username}`);
+}
+
+function register() {
+  alert("Registration functionality not implemented yet.");
+}
